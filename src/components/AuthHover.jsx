6d901dd0a4b53c13ebeed26b72ba56e47311d4bc
Omar Adrian <oadrian38@gmail.com>
2024-03-01 15:25:39 -0600

    add AuthHover that manages login and signup. Profile info if authtenticated

---
+++ src/components/AuthHover.jsx
@@ -0,0 +1,12 @@
+const AuthHover = ({mode}) => {
+    return(
+        <>
+            <div className="profileHover profileHover--noauth">
+                <input type="button" value="Registrarse" />
+                <input type="button" value="iniciar sesion" />
+            </div>
+        </>
+    )
+}
+
+export default AuthHover
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,14 @@
 import monadocumentos from '../assets/monadocumento.png'
+import AuthHover from './AuthHover';
 
 const Body = () => {
     return(
         <>
             <div className="body">
-                <div className="body_container">
+                <AuthHover />
+                <div className="landingContainer__body">
                     <img src={monadocumentos} alt="Imagen de una mona con un documento"/>
-                    <div className="body_container_data">
+                    <div className="landingContainer__info">
                         <h3>Obten tus documentos de servicio social</h3>
                         <input className='btn btn_captureData' type="button" value="Captura tus datos" />
                     </div>
