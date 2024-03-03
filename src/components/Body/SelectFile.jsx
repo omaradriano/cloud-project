96b0a3d87e90162293c4b7d73a4d868699b96fc6
Omar Adrian <oadrian38@gmail.com>
2024-03-03 16:50:42 -0600

    add styles for view_select_file

--- src/components/Body/SelectFile.jsx
+++ src/components/Body/SelectFile.jsx
@@ -1,28 +1,10 @@
 import { useState } from 'react'
-import autoevaluacion_cualitativa from '../../assets/FilesImages/formulario_autoevaluacion_cualitativa_PRESTADOR.png'
-import carta_compromiso from '../../assets/FilesImages/formulario_carta_compromiso.png'
-import actividades from '../../assets/FilesImages/formulario_de_actividades_PRESTADOR.png'
-import carta_asignacion from '../../assets/FilesImages/formulario_de_carta_de_asignacion.png'
-import reporte_bimestral from '../../assets/FilesImages/formulario_de_reporte_bimestral.png'
-import solicitud from '../../assets/FilesImages/formulario_de_solicitud.png'
-import evaluacion_cualitativa from '../../assets/FilesImages/formulario_evaluacion_cualitativa_INSTITUCION.png'
-import plan_de_trabajo from '../../assets/FilesImages/formulario_plan_de_trabajo.png'
-
+import files from '../../utils/files'
+import { Link } from 'react-router-dom';
 const SelectFile = () => {
 
     const [file, setFile] = useState('file_1')
 
-    const files = {
-        "file_1": autoevaluacion_cualitativa,
-        "file_2": evaluacion_cualitativa,
-        "file_3": carta_compromiso,
-        "file_4": actividades,
-        "file_5": carta_asignacion,
-        "file_6": reporte_bimestral,
-        "file_7": solicitud,
-        "file_8": plan_de_trabajo,
-        
-    }
     function handleSelectChange(event){
         setFile(event.target.value)
         // console.log(event.target.value)
@@ -46,7 +28,8 @@ const SelectFile = () => {
                     <img src={files[file]} alt="Imagen de documento" />
                 </div>
 
-                <input className='btn' type="button" value="Aceptar y llenar documento" />
+                <Link className='btn' to={'/linkdellenado'}>Aceptar y llenar documento</Link>
+                {/* <input className='btn' type="button" value="Aceptar y llenar documento" /> */}
             </div>
         </>
     )
---
+++ src/utils/files.js
@@ -0,0 +1,22 @@
+import autoevaluacion_cualitativa from '../assets/FilesImages/formulario_autoevaluacion_cualitativa_PRESTADOR.png'
+import carta_compromiso from '../assets/FilesImages/formulario_carta_compromiso.png'
+import actividades from '../assets/FilesImages/formulario_de_actividades_PRESTADOR.png'
+import carta_asignacion from '../assets/FilesImages/formulario_de_carta_de_asignacion.png'
+import reporte_bimestral from '../assets/FilesImages/formulario_de_reporte_bimestral.png'
+import solicitud from '../assets/FilesImages/formulario_de_solicitud.png'
+import evaluacion_cualitativa from '../assets/FilesImages/formulario_evaluacion_cualitativa_INSTITUCION.png'
+import plan_de_trabajo from '../assets/FilesImages/formulario_plan_de_trabajo.png'
+
+const files = {
+    "file_1": autoevaluacion_cualitativa,
+    "file_2": evaluacion_cualitativa,
+    "file_3": carta_compromiso,
+    "file_4": actividades,
+    "file_5": carta_asignacion,
+    "file_6": reporte_bimestral,
+    "file_7": solicitud,
+    "file_8": plan_de_trabajo,
+    
+}
+
+export default files
